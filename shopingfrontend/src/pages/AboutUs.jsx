1458827a3831ff34613e4a960762692b0baf3048
Hemanth <hemanth.4324@gmail.com>
2023-04-24 19:27:23 +0530

    adding frontend files

---
+++ shopingfrontend/src/pages/AboutUs.jsx
@@ -0,0 +1,26 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+        Welcome to our e-commerce platform, where we are dedicated to providing a seamless online shopping experience to our customers. Our platform is designed with your convenience in mind, allowing you to browse and purchase goods and services from the comfort of your own home.
+        <br />
+        <br />
+        We believe in the power of e-commerce to connect buyers and sellers from all around the world, eliminating geographical barriers and enabling businesses to reach a global audience. Our platform is built with the latest technology to ensure that your transactions are secure and your information is kept safe.
+        <br />
+        <br />
+
+        At our core, we are committed to providing excellent customer service and fostering long-lasting relationships with our clients. Our team is always here to assist you with any questions or concerns you may have, and we strive to make your shopping experience as smooth and enjoyable as possible.
+        
+      <br/> 
+      <br />
+      
+
+      Thank you for choosing our e-commerce platform for your shopping needs. We look forward to serving you and making your online shopping experience a truly exceptional one.
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
